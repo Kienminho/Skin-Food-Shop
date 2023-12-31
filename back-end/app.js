cb84503e4d0ea605f3a4efa86e3495078f0b6701
Trần Hợp Kiên <hopkien1609@gmail.com>
2023-12-31 10:43:47 +0700

    add redis store

--- back-end/app.js
+++ back-end/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const session = require("express-session");
-const RedisStore = require("connect-redis")(session);
-const cookieParser = require("cookie-parser");
-
 require("dotenv").config();
+
+const client = require("./common/redisConnect");
+const RedisStore = require("connect-redis").default;
+
 const app = express();
 
 //middleware
@@ -14,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
   session({
-    store: new RedisStore({ client: redisClient }),
+    store: new RedisStore({ client: client }),
     secret: "my-key",
     resave: true,
     saveUninitialized: true,
