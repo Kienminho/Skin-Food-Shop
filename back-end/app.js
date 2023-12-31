c42e2fac4c5be76d6130f8254db55bee64e063ee
Trần Hợp Kiên <hopkien1609@gmail.com>
2023-12-31 11:07:12 +0700

    trust poxy

--- back-end/app.js
+++ back-end/app.js
@@ -12,6 +12,7 @@ const RedisStore = require("connect-redis").default;
 const app = express();
 
 //middleware
+app.set("trust proxy", 1);
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
